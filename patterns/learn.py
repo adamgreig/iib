6a6a20c56e5b1f404d9cda3d758c7d7936635377
Adam Greig <adam@adamgreig.com>
2013-05-09 18:04:06 +0100

    add some validation items

--- patterns/learn.py
+++ patterns/learn.py
@@ -1,22 +1,41 @@
+import yaml
+import features
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import mixture
+from skimage import io
 
 def main():
     train = np.load("corpus/patterns.npy")
     reject = np.load("corpus/nopatterns.npy")
-    clf = mixture.GMM(n_components=3, covariance_type='full', n_iter=1000,
-                      n_init=100)
+    clf = mixture.GMM(n_components=2, covariance_type='full', n_iter=2000,
+                      n_init=500)
     print("Fitting model...")
     clf.fit(train)
-    pattern_scores = clf.score(train).astype(np.int)
-    reject_scores = clf.score(reject).astype(np.int)
-    print("Score for training data:", pattern_scores)
-    print("Score for reject data:", reject_scores)
+    print("Fitted, computing scores on examples...")
 
-    plt.scatter(pattern_scores, np.zeros_like(pattern_scores), c='g')
-    plt.scatter(reject_scores, np.zeros_like(reject_scores), c='r')
+    with open("corpus/manifest.yaml") as f:
+        manifest = yaml.load(f)
+
+    scored_paths = []
+    for cls in manifest.keys():
+        for img in sorted(manifest[cls].keys()):
+            path = "corpus/"+manifest[cls][img]["path"]
+            x = features.edge_stats(path) + features.blob_stats(path)
+            score = int(clf.score(np.array(x).reshape((1, 7)))[0])
+            scored_paths.append((score, path))
+
+    for idx, (score, path) in enumerate(reversed(sorted(scored_paths))):
+        img = io.imread(path)
+        rows = int(np.ceil(np.sqrt(len(scored_paths))))
+        plt.subplot(rows, rows, idx+1)
+        plt.imshow(img)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(str(score))
+
     plt.show()
+            
 
 if __name__ == "__main__":
     main()
