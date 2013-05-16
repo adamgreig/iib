3fbc0403deb9485b0d0862af5a49da5bd0535070
Adam Greig <adam@adamgreig.com>
2013-05-16 05:33:15 +0100

    starting to get non-stable patterns...

--- simulation/genome.py
+++ simulation/genome.py
@@ -3,7 +3,7 @@ from string import Template
 
 genome_cl_str = """//CL//
 // A slightly leaky activation function that saturates at about x=1.0
-#define activation(x) native_recip(1.0f + native_exp(4.0f - 10.0f * x))
+#define activation(x) native_recip(1.0f + native_exp(5.0f - 20.0f * x))
 #define POS (get_global_id(1)*get_global_size(0) + get_global_id(0))
 
 __kernel void genome(__global float* sigs_in, __global float* sigs_out)
@@ -21,11 +21,13 @@ __kernel void genome(__global float* sigs_in, __global float* sigs_out)
 
 Gene = namedtuple('Gene', 'reg sig_in rbs sig_out deg')
 
+
 def parsegenes(genestr):
     genes = []
     for g in [genestr[i:i+5] for i in range(0, len(genestr), 5)]:
         genes.append(Gene(g[0], g[1], int(g[2]), g[3], int(g[4])))
     return genes
+
 
 def degcode(genes, signal):
     rbs_deg = [(g.rbs, g.deg) for g in genes if g.sig_out == signal]
@@ -37,6 +39,7 @@ def degcode(genes, signal):
     factor = 1.0 - deg_avg
     return "sigs.s{sig} *= {factor}f;".format(sig=signal, factor=factor)
 
+
 def prodcode(gene):
     if gene.rbs == 0:
         return ""
@@ -45,7 +48,8 @@ def prodcode(gene):
         act = "(1.0f - {act})".format(act=act)
     amt = "{act} * {rbs:0.1f}f".format(act=act, rbs=0.1*gene.rbs)
     return "sigs.s{out} += {amt};".format(out=gene.sig_out, amt=amt)
-    
+
+
 def genome_cl(genestr):
     genes = parsegenes(genestr)
     signals = [str(i) for i in range(10)] + [chr(c) for c in range(65, 71)]
--- simulation/simulation.py
+++ simulation/simulation.py
@@ -7,24 +7,30 @@ import pyopencl as cl
 
 from PIL import Image
 
+
 def main():
     wg_size = 128
-    g_size  = 512
+    g_size = 512
 
     ctx = cl.create_some_context(interactive=False)
     queue = cl.CommandQueue(ctx)
     mf = cl.mem_flags
-    
+
     #sigs_a = np.random.random(g_size*g_size*16).astype(np.float32) * 0.8
     sigs_a = np.zeros((g_size, g_size, 16), np.float32)
-    sigs_a[206:306,206:306,0] = 1.0
-    sigs_a[0:512  ,0:206  ,1] = 1.0
-    sigs_a[0:512  ,307:512,1] = 1.0
-    sigs_a[0:206  ,0:512  ,1] = 1.0
-    sigs_a[307:512,0:512  ,1] = 1.0
+    #sigs_a[:, :, 10] = np.random.random((g_size, g_size)).astype(np.float32)
+    #sigs_a[:, :, 10] *= 0.2
+    sigs_a[:, :, 10] = np.random.randint(
+        0, 2, (g_size, g_size)).astype(np.float32)
+    #sigs_a[206:306, 206:306, 10] = 0.8
+    #sigs_a[0:512, 0:206, 1] = 1.0
+    #sigs_a[0:512, 307:512, 1] = 1.0
+    #sigs_a[0:206, 0:512, 1] = 1.0
+    #sigs_a[307:512, 0:512, 1] = 1.0
+    sigs_a[:, :, 1] = 0.5
     sigs_a = sigs_a.reshape(g_size * g_size * 16)
     sigs_b = np.empty(g_size*g_size*16, np.float32)
-    
+
     buf_a = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=sigs_a)
     buf_b = cl.Buffer(ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=sigs_b)
 
@@ -36,28 +42,41 @@ def main():
         bsig = str(chr(sig)).encode()
         program.colour(queue, (g_size, g_size), None, buf, bsig, ibuf)
         cl.enqueue_copy(
-            queue, image, ibuf, origin=(0,0), region=(g_size, g_size)).wait()
+            queue, image, ibuf, origin=(0, 0), region=(g_size, g_size)).wait()
         img = Image.fromarray(image.reshape((g_size, g_size, 4)))
         img.save("output/{0}_{1:05d}.png".format(sig, iteration))
 
-    test_genome = "+0207-1609-0313"
-    test_sigmas = [0.9, 5.0] * 8
+    def print_signals(buf, sigs):
+        cl.enqueue_copy(queue, sigs_a, buf).wait()
+        for sig in sigs:
+            print("Sig {0:X}:".format(sig), end=' ')
+            v = sigs_a.reshape(g_size, g_size, 16)[0, 0, sig]
+            print("{0:0.6f}".format(float(v)), end='\t')
+        print()
+
+    test_genome = "+A303+A513-12A2"
+    test_sigmas = [1.5, 3.0] * 4 + [0.0] * 8
     progstr = genome.genome_cl(test_genome)
+    print(progstr)
     progstr += diffusion.diffusion_cl(test_sigmas, wg_size)
     progstr += colour.colour_cl()
     program = cl.Program(ctx, progstr).build()
 
-    dump_image(buf_a, 0, 0)
-    dump_image(buf_a, 1, 0)
+    dump_image(buf_a,  0, 0)
+    dump_image(buf_a,  1, 0)
+    dump_image(buf_a, 10, 0)
+    print_signals(buf_a, (0, 1, 10))
 
-    iterations = 500
+    iterations = 100
     for iteration in range(iterations):
         program.genome(queue, (g_size, g_size), None, buf_a, buf_b).wait()
         program.diffusion(queue, (g_size, g_size), (wg_size, 1), buf_b, buf_a)
-        program.diffusion(queue, (g_size, g_size), (1, wg_size), buf_a, buf_b).wait()
+        program.diffusion(queue, (g_size, g_size), (1, wg_size), buf_a, buf_b)
         buf_a, buf_b = buf_b, buf_a
-        dump_image(buf_a, 0, iteration+1) 
-        dump_image(buf_a, 1, iteration+1) 
+        dump_image(buf_a,  0, iteration+1)
+        dump_image(buf_a,  1, iteration+1)
+        dump_image(buf_a, 10, iteration+1)
+        print_signals(buf_a, (0, 1, 10))
 
 
 if __name__ == "__main__":
