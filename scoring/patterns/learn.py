83d1a1cbd5417c1903194c7797b355e0ce78cadf
Adam Greig <adam@adamgreig.com>
2013-05-18 04:16:47 +0100

    add trained model pickling

--- scoring/patterns/learn.py
+++ scoring/patterns/learn.py
@@ -1,4 +1,5 @@
 import yaml
+import pickle
 import features
 import numpy as np
 import scipy.stats
@@ -14,108 +15,123 @@ cls_lut = {
 }
 
 
-def gkde_train(positive, negative, validate):
-    return scipy.stats.gaussian_kde(positive.T)
+class gkde:
+    def train(self, positive, negative, validate):
+        self.pdf = scipy.stats.gaussian_kde(positive.T)
+
+    def score(self, x):
+        return np.log(self.pdf(x))
 
 
-def gkde_score(model, x):
-    return np.log(model(x))
+class gmm:
+    def train(self, positive, negative, validate):
+        self.clf = mixture.GMM(n_components=3, covariance_type='full',
+                               n_iter=2000, n_init=500)
+        self.clf.fit(positive)
+
+    def score(self, x):
+        return self.clf.score(x)
 
 
-def gmm_train(positive, negative, validate):
-    clf = mixture.GMM(n_components=3, covariance_type='full', n_iter=2000,
-                      n_init=500)
-    clf.fit(positive)
-    return clf
+class ocsvm:
+    def train(self, positive, negative, validate):
+        self.scaler = preprocessing.StandardScaler().fit(positive)
+        X = self.scaler.transform(positive)
+        self.clf = svm.OneClassSVM(nu=0.1, gamma=0.1, cache_size=512)
+        self.clf.fit(X)
+
+    def score(self, x):
+        d = self.clf.decision_function(self.scaler.transform(x))
+        d[d > 0] = 0.0
+        d[d < 0] *= 100.0
+        return d
 
 
-def gmm_score(model, x):
-    return model.score(x)
+class rf:
+    def train(self, positive, negative, validate):
+        X = np.vstack((positive, negative))
+        y = [1]*positive.shape[0] + [0]*negative.shape[0]
+        self.clf = ensemble.RandomForestClassifier(n_estimators=1000)
+        self.clf.fit(X, y)
+
+    def score(self, x):
+        return self.clf.predict_log_proba(x)[:, 1]
 
 
-def ocsvm_train(positive, negative, validate):
-    scaler = preprocessing.StandardScaler().fit(positive)
-    X = scaler.transform(positive)
-    clf = svm.OneClassSVM(nu=0.1, gamma=0.1, cache_size=512)
-    clf.fit(X)
-    return [scaler, clf]
+models = [
+    ["(Train all and save results)"],
+    ["Gaussian Kernel Density Estimate", gkde],
+    ["Gaussian Mixture Model", gmm],
+    ["One-Class Support Vector Machine", ocsvm],
+    ["Random Forest", rf]
+]
 
 
-def ocsvm_score(model, x):
-    d = model[1].decision_function(model[0].transform(x))
-    d[d > 0] = 0.0
-    d[d < 0] *= 100.0
-    return d
-
-
-def rf_train(positive, negative, validate):
-    X = np.vstack((positive, negative))
-    y = [1]*positive.shape[0] + [0]*negative.shape[0]
-    clf = ensemble.RandomForestClassifier()
-    return clf.fit(X, y)
-
-
-def rf_score(model, x):
-    return model.predict_log_proba(x)[:, 1]
-
-
-models = {
-    "Gaussian Kernel Density Estimate": [gkde_train, gkde_score],
-    "Gaussian Mixture Model": [gmm_train, gmm_score],
-    "One-Class Support Vector Machine": [ocsvm_train, ocsvm_score],
-    "Random Forest": [rf_train, rf_score]
-}
-
-
-def learn(train_cb, score_cb, modelname):
+def learn(model):
     print("Loading feature data...")
     positive = np.load("corpus/patterns.npy")
     negative = np.load("corpus/nopatterns.npy")
     validate = np.load("corpus/validation.npy")
     print("Fitting model...")
-    model = train_cb(positive, negative, validate)
-    print("Fitted, computing scores on examples...")
+    model.train(positive, negative, validate)
 
+
+def score(model):
+    print("Computing scores on examples...")
     with open("corpus/manifest.yaml") as f:
         manifest = yaml.load(f)
-
-    scored_paths = []
+    scores = []
     for cls in manifest.keys():
         for img in sorted(manifest[cls].keys()):
             path = "corpus/"+manifest[cls][img]["path"]
             x = features.edge_stats(path) + features.blob_stats(path)
-            score = float(score_cb(model, np.array(x).reshape((1, 7))))
-            scored_paths.append((score, path, cls))
+            score = float(model.score(np.array(x).reshape((1, 7))))
+            scores.append((score, path, cls))
+    return scores
 
-    for idx, (score, path, cls) in enumerate(reversed(sorted(scored_paths))):
+
+def plot(scores, modelname):
+    print("Plotting scores and test images...")
+    for idx, (score, path, cls) in enumerate(reversed(sorted(scores))):
         img = io.imread(path)
-        rows = int(np.ceil(np.sqrt(len(scored_paths))))
+        rows = int(np.ceil(np.sqrt(len(scores))))
         ax = plt.subplot(rows, rows, idx+1)
         plt.imshow(img)
         plt.xticks([])
         plt.yticks([])
-        t = plt.title("{0:.5g}".format(score))
+        t = plt.title("{0:g}".format(score))
         for s in ("bottom", "top", "left", "right"):
             col = cls_lut.get(cls, 'k')
             ax.spines[s].set_color(col)
             t.set_color(col)
 
-    plt.suptitle("Scores for {0} (blue=train, red=reject, green=validation)"
+    plt.suptitle("Scores for {0} (blue=training, red=reject, green=validation)"
                  .format(modelname))
     plt.show()
 
 
 def main():
     print("Select model:")
-    for i, k in enumerate(models.keys()):
-        print("[{0}] {1}".format(i+1, k))
-    m = int(input("> ")) - 1
-    if not 0 <= m <= len(models.keys()):
+    for i in range(len(models)):
+        print("[{0}] {1}".format(i, models[i][0]))
+    m = int(input("> "))
+    if not 0 <= m < len(models):
         print("Invalid selection.")
         return
-    k = list(models.keys())[m]
-    learn(models[k][0], models[k][1], k)
-
+    if m != 0:
+        model = models[m][1]()
+        learn(model)
+        scores = score(model)
+        plot(scores, models[m][0])
+    else:
+        trained_models = []
+        for m in models[1:]:
+            print("Training", m[0])
+            model = m[1]()
+            learn(model)
+            trained_models.append(model)
+        with open("corpus/trained_models.p", "wb") as f:
+            pickle.dump(trained_models, f)
 
 if __name__ == "__main__":
     main()
