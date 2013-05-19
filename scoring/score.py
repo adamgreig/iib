b676f8421ac4799df80a1ea708eb3b4801a70576
Adam Greig <adam@adamgreig.com>
2013-05-19 06:18:25 +0100

    add ability to preload the trained models

--- scoring/score.py
+++ scoring/score.py
@@ -5,11 +5,21 @@ import numpy as np
 from skimage.transform import resize
 
 
-def score(signals):
+trained_models = None
+
+
+def preload_models():
+    global trained_models
     with open(corpus_path("trained_models.p"), "rb") as f:
-        models = pickle.load(f)
+        trained_models = pickle.load(f)
+
+
+def score(signals):
+    global trained_models
+    if not trained_models:
+        preload_models()
     x = [stats(resize(signals[:, :, s], (64, 64))) for s in range(16)]
-    scores = np.array([m.score(np.array(x)) for m in models])
+    scores = np.array([m.score(np.array(x)) for m in trained_models])
     return np.amax(scores, axis=1)
 
 
