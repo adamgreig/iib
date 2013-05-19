ffb9be3a471847cae0b094fa76bf13c892f8a0f2
Adam Greig <adam@adamgreig.com>
2013-05-19 05:24:14 +0100

    tidy up scoring code

--- client/process.py
+++ client/process.py
@@ -1,19 +1,25 @@
 from iib.simulation import run
 from iib.scoring import score
 
-#import numpy as np
+import copy
 
 
 def process(config):
     signals = run.run_simulation(config)
-    scores = score.score(signals, only_highest_signal=True)
+    scores = score.score(signals)
     return scores
 
-test_config = {
+
+def process_genome(genome):
+    cfg = copy.copy(standard_config)
+    cfg["genome"] = genome
+    return process(cfg)
+
+
+standard_config = {
     "grid_size": 512,
     "wg_size": 128,
     "iterations": 100,
-    "genome": "+A303+A563-62A2",
     "signals": [
         {"diffusion": 1.0, "initial": 0.0},
         {"diffusion": 1.0, "initial": 0.0},
@@ -25,19 +31,15 @@ test_config = {
         {"diffusion": 3.0, "initial": 0.0},
         {"diffusion": 5.0, "initial": 0.0},
         {"diffusion": 5.0, "initial": 0.0},
-        {"diffusion": 0.0, "initial": "random_binary"},
-        {"diffusion": 0.0, "initial": 0.0},
-        {"diffusion": 0.0, "initial": 0.0},
-        {"diffusion": 0.0, "initial": 0.0},
-        {"diffusion": 0.0, "initial": 0.0},
-        {"diffusion": 0.0, "initial": 0.0}
+        {"diffusion": 0.0, "initial": "random_float"},
+        {"diffusion": 0.0, "initial": "random_float"},
+        {"diffusion": 0.0, "initial": "random_float"},
+        {"diffusion": 0.0, "initial": "random_float"},
+        {"diffusion": 0.0, "initial": "random_float"},
+        {"diffusion": 0.0, "initial": "random_float"}
     ]
 }
 
 
-def main():
-    print(process(test_config))
-
-
 if __name__ == "__main__":
-    main()
+    print(process_genome("+A303+A563-62A2"))
--- scoring/score.py
+++ scoring/score.py
@@ -1,29 +1,18 @@
-from iib.scoring.features import corpus_path, stats as features_stats
+from iib.scoring.features import corpus_path, stats
 
 import pickle
 import numpy as np
-from skimage import transform
+from skimage.transform import resize
 
 
-def score(signals, only_highest_signal=False):
+def score(signals):
     with open(corpus_path("trained_models.p"), "rb") as f:
         models = pickle.load(f)
-    x = []
-    for s in range(16):
-        im = signals[:, :, s]
-        im = transform.resize(im, (64, 64))
-        x.append(features_stats(im))
-    scores = []
-    x = np.array(x)
-    for model in models:
-        score = model.score(x)
-        if only_highest_signal:
-            score = np.max(score)
-        scores.append(score)
-    return np.array(scores)
+    x = [stats(resize(signals[:, :, s], (64, 64))) for s in range(16)]
+    scores = np.array([m.score(np.array(x)) for m in models])
+    return np.amax(scores, axis=1)
 
 
 if __name__ == "__main__":
     sigs = np.zeros((512, 512, 16), np.float32)
     print(score(sigs))
-    print(score(sigs, True))
