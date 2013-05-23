e013e2ba3d46aefb9ce1c04abc0259bde7c260b4
Adam Greig <adam@adamgreig.com>
2013-05-23 09:40:43 +0100

    move CLContext around to avoid circular imports

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -1,15 +1,7 @@
+from .clcontext import CLContext
 from . import colour
 from . import convolution
 from . import genome
 from . import reduction
 from . import features
 from . import run
-
-
-class CLContext:
-    def __init__(self, ctx, queue, ifmt, gs, wgs):
-        self.ctx = ctx
-        self.queue = queue
-        self.ifmt = ifmt
-        self.gs = gs
-        self.wgs = wgs
---
+++ simulation/clcontext.py
@@ -0,0 +1,7 @@
+class CLContext:
+    def __init__(self, ctx, queue, ifmt, gs, wgs):
+        self.ctx = ctx
+        self.queue = queue
+        self.ifmt = ifmt
+        self.gs = gs
+        self.wgs = wgs
