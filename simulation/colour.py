661ade9544502314b3d689b5d4adcb75bb57fe82
Adam Greig <adam@adamgreig.com>
2013-05-23 09:40:57 +0100

    add execution function to colour.py

--- simulation/colour.py
+++ simulation/colour.py
@@ -1,5 +1,7 @@
 import colorsys
 import numpy as np
+import pyopencl as cl
+from PIL import Image
 from string import Template
 
 colour_cl_str = """//CL//
@@ -32,5 +34,40 @@ def colour_cl():
     return Template(colour_cl_str).substitute(colourlut=colour_lut)
 
 
+def dump_colour_image(clctx, colours, bufi, channel, fpath):
+    gs, wgs = clctx.gs, clctx.wgs
+    ifmt_u = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.UNORM_INT8)
+    buf = cl.Image(clctx.ctx, cl.mem_flags.WRITE_ONLY, ifmt_u, (gs, gs))
+    img = np.empty(gs*gs*4, np.uint8)
+    channel = str(chr(channel)).encode()
+    colours.colour(clctx.queue, (gs, gs), (wgs, wgs), bufi, channel, buf)
+    cl.enqueue_copy(clctx.queue, img, buf, origin=(0, 0), region=(gs, gs))
+    img = img.reshape((gs, gs, 4))
+    img = Image.fromarray(img)
+    img.save(fpath)
+
+
+def test():
+    from iib.simulation import CLContext
+    gs, wgs = 256, 16
+    ctx = cl.create_some_context(interactive=False)
+    queue = cl.CommandQueue(ctx)
+    ifmt_f = cl.ImageFormat(cl.channel_order.RGBA, cl.channel_type.FLOAT)
+    bufi = cl.Image(ctx, cl.mem_flags.READ_ONLY, ifmt_f, (gs, gs))
+    colours = cl.Program(ctx, colour_cl()).build()
+    clctx = CLContext(ctx, queue, ifmt_f, gs, wgs)
+
+    x = np.arange(-128., 128.)/30
+    xx = np.outer(x, x)
+    y = np.sinc(xx)
+    sigs = np.empty((gs, gs, 4), np.float32)
+    sigs[:, :, 0] = y
+    sigs = sigs.reshape(gs*gs*4)
+    cl.enqueue_copy(queue, bufi, sigs, origin=(0, 0), region=(gs, gs))
+
+    dump_colour_image(clctx, colours, bufi, 0, "testimage.png")
+    print("Test image dumped to ./testimage.png")
+
 if __name__ == "__main__":
     print(colour_cl())
+    test()
