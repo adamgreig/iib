f8fa92a687afb2181c36e5e10b2b6af23ae2407b
Adam Greig <adam@adamgreig.com>
2013-05-24 04:43:25 +0100

    clean up commented out code

--- client/worker.py
+++ client/worker.py
@@ -4,7 +4,6 @@ from rq import Connection, Queue, Worker
 
 # Preload libraries
 import iib
-#iib.scoring.score.preload_models()
 
 with Connection(Redis(decode_responses=True)):
     Worker([Queue()]).work()
