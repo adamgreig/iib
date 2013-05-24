1a6d95d166822d916c03d3a16703d69f1a6fe1d7
Adam Greig <adam@adamgreig.com>
2013-05-24 10:10:56 +0100

    basic selection/evolution code

---
+++ server/evolution.py
@@ -0,0 +1,94 @@
+from iib.server import operators
+import multiprocessing
+import numpy as np
+import random
+
+
+def random_genome():
+    """
+    Make a random genome such that at least one diffusing and one non-diffusing
+    signal are expressed.
+    """
+    genes = []
+    while True:
+        genes.append(operators.random_gene())
+        s = {int(g[3])//4 for g in genes}
+        if 0 in s and 1 in s:
+            break
+    return ''.join(genes)
+
+
+def first_generation(n):
+    """Create the initial generation of *n* individuals."""
+    return [random_genome() for i in range(n)]
+
+
+def select_parents(n, generation):
+    """
+    Use stochastic universal sampling to select *n* parents from *generation*.
+    """
+    generation = sorted(generation, key=lambda i: float(i[1]), reverse=True)
+    scores = np.array([float(individual[1]) for individual in generation])
+    scores /= np.sum(scores)
+    scores = np.add.accumulate(scores)
+    position = random.uniform(0, 1/n)
+    parents = []
+    for idx, score in enumerate(scores):
+        if score > position:
+            parents.append(generation[idx][0])
+            position += 1/n
+    return parents
+
+
+def crossover(parents):
+    """Create a new genome by crossing over the two *parents*."""
+    return random.choice(operators.crossovers)(parents)
+
+
+def mutate(child):
+    """Modify the single *child* genome by mutating it."""
+    return random.choice(operators.mutators)(child)
+
+
+def new_child(parents):
+    myparents = random.sample(parents, 2)
+    return mutate(crossover(myparents))
+
+
+def new_generation(old):
+    """Create a new generation from *old* of the same size."""
+    n_parents = len(old) / 10
+    parents = select_parents(n_parents, old)
+    children = []
+    results = []
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+    for _ in range(len(old)):
+        results.append(pool.apply_async(new_child, [parents]))
+    pool.close()
+    pool.join()
+    children = [result.get() for result in results]
+    return children
+
+
+if __name__ == "__main__":
+    import time
+    import matplotlib.pyplot as plt
+
+    def fake_scores(generation):
+        out = []
+        for i in generation:
+            out.append([i, random.randrange(100)])
+        return out
+
+    lengths = []
+    t0 = time.time()
+    cohort = fake_scores(first_generation(1000))
+    iters = 100
+    for i in range(iters):
+        cohort = fake_scores(new_generation(cohort))
+        lengths.append(sum(len(i[0])/5 for i in cohort) / len(cohort))
+        t = time.time() - t0
+        print("iter {0}, {1:.2f}, {2:.2f}s".format(i, lengths[-1], t))
+    print("Average time/iter: {0:.2f}".format((time.time() - t0)/iters))
+    plt.plot(lengths)
+    plt.show()
---
+++ server/operators.py
@@ -0,0 +1,108 @@
+import random
+
+
+def parse_genes(genome):
+    genes = []
+    for g in [genome[i:i+5] for i in range(0, len(genome), 5)]:
+        genes.append(g)
+    return genes
+
+
+def random_gene():
+    reg = random.choice(('+', '-'))
+    s0, w0 = str(random.randrange(8)), str(random.randrange(10))
+    s1, w1 = str(random.randrange(8)), str(random.randrange(10))
+    return ''.join((reg, s0, w0, s1, w1))
+
+
+def mutate_add_gene(genome):
+    return genome + random_gene()
+
+
+def mutate_del_gene(genome):
+    genes = parse_genes(genome)
+    if len(genes) == 1:
+        return mutate_add_gene(genome)
+    i = random.randrange(len(genes))
+    del genes[i]
+    return ''.join(genes)
+
+
+def mutate_adddel_gene(genome):
+    n = len(genome)//5
+    k = random.expovariate(1/3.0)
+    if k > n:
+        return mutate_add_gene(genome)
+    else:
+        return mutate_del_gene(genome)
+
+
+def mutate_flip_regulation(genome):
+    genes = parse_genes(genome)
+    i = random.randrange(len(genes))
+    oldgene = genes[i]
+    reg = '+' if oldgene[0] == '-' else '-'
+    genes[i] = reg + oldgene[1:]
+    return ''.join(genes)
+
+
+def mutate_change_signal(genome):
+    genes = parse_genes(genome)
+    i = random.randrange(len(genes))
+    oldgene = genes[i]
+    idx = random.choice((1, 3))
+    newsig = str(random.randrange(8))
+    newgene = oldgene[:idx] + newsig + oldgene[idx+1:]
+    genes[i] = newgene
+    return ''.join(genes)
+
+
+def mutate_change_weight(genome):
+    genes = parse_genes(genome)
+    i = random.randrange(len(genes))
+    oldgene = genes[i]
+    idx = random.choice((2, 4))
+    newweight = oldweight = float(oldgene[idx])
+    while newweight == oldweight:
+        newweight = max(0, min(9, int(round(random.gauss(oldweight, 1)))))
+    newgene = oldgene[:idx] + str(newweight) + oldgene[idx+1:]
+    genes[i] = newgene
+    return ''.join(genes)
+
+
+mutators = ([mutate_adddel_gene] * 2 + [mutate_flip_regulation] +
+            [mutate_change_signal] * 3 + [mutate_change_weight] * 4)
+
+
+def crossover_concatenate(parents):
+    """
+    Concatenate genomes, then knock out half the difference in the number
+    of genes between the two parents, so the child will have the mean number of
+    genes.
+    """
+    genome = ''.join(parents)
+    newlen = (len(parents[0]) + len(parents[1]))//5
+    target = newlen // 2 + random.randint(0, 1)
+    knockouts = newlen - target
+    for i in range(knockouts):
+        genome = mutate_del_gene(genome)
+    return genome
+
+
+def crossover_replace(parents):
+    """
+    Swap half the genes in the second parent with genes in the first parent
+    to produce a child.
+    """
+    genes_a = parse_genes(parents[0])
+    genes_b = parse_genes(parents[1])
+    swaps = len(genes_b)//2
+    for _ in range(swaps):
+        i = random.randrange(len(genes_a))
+        j = random.randrange(len(genes_b))
+        genes_a[i] = genes_b[j]
+        del genes_b[j]
+    return ''.join(genes_a)
+
+
+crossovers = [crossover_concatenate, crossover_replace]
